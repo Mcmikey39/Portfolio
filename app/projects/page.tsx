4541bd5304ccf141c91927d1a729006abc9196b8
Michael Bloemraad <mikebloemraad@gmail.com>
2025-05-25 22:12:13 +0200

    Add hero wave animation and navigation logo SVG component
    - Implemented a new Herowave component that features an animated wave using framer-motion.
    - The wave transitions between three different paths every 3 seconds.
    - Added a linear gradient for a fading effect on the wave.
    - Created a navLogo component that renders a simple SVG logo.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,5 @@
 import Cards from "@/components/cards";
-import Herowave from "@/images/svg/heroWave";
+import Herowave from "@/app/public/images/svg/heroWave";
 
 export default function Projects() {
   return (
